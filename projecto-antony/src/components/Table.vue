30c1105540d903d54ba671156709657c9a361e5c
TonyRoda <richerdantony5@gmail.com>
2025-05-09 19:12:24 -0500

    objeto reutillizable

---
+++ projecto-antony/src/components/Table.vue
@@ -0,0 +1,22 @@
+<template>
+    <table>
+        <thead>
+            <tr>
+                <th v-for="col in columns" :key="col" class="p-2 border-b capitallize">
+                    {{ col }}
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>
+                    {{ item[col] }}
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+<script>
+
+</script>
